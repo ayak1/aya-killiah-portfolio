f1606c0cf6e39b5414b61e76909aa33eca5cc15c
ayak1 <ayakilliah2163@gmail.com>
2024-12-29 15:22:37 +0300

    edit content on hero and about

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/3D-Space-Hero",
+  base: "/aya-killiah-portfolio",
   plugins: [react()],
 })
  
